d50156df5d0779bd31204a38da3738587b23bc3f
jamirul <jamirul.developer@gmail.com>
2024-07-31 23:19:50 +0530

    json extra added.

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -2,4 +2,5 @@ return {
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.tailwind" },
   { import = "lazyvim.plugins.extras.lang.astro" },
+  { import = "lazyvim.plugins.extras.lang.json" },
 }
