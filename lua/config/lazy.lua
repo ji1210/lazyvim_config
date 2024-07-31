82e56a187538422246d354daa1e0f220549b5af7
jamirul <jamirul.developer@gmail.com>
2024-07-31 23:17:19 +0530

    organised extras to separate file.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,9 +18,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.tailwind" },
-    { import = "lazyvim.plugins.extras.lang.astro" },
     -- import/override with your plugins
     { import = "plugins" },
   },
