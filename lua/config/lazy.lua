63ae14fe8c11188525447027895fedd8fe9faf63
jamirul <jamirul.developer@gmail.com>
2024-07-31 21:49:10 +0530

    added lazy extras tailwind, astro, typescript

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,9 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.astro" },
     -- import/override with your plugins
     { import = "plugins" },
   },
